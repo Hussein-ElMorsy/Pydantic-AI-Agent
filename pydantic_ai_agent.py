692a9338e421def0b3c32e463cc797318a16ddd8
Hussein El-Morsy <hussein.el-morsy@ibm.com>
2025-11-04 16:15:03 +0200

    Implement the logic

--- pydantic_ai_agent.py
+++ pydantic_ai_agent.py
@@ -1,5 +1,12 @@
 import requests
+from dotenv import load_dotenv, find_dotenv
+from pydantic import BaseModel
+from pydantic_ai import Agent, RunContext
+from pydantic_ai.settings import ModelSettings
 
+_ = load_dotenv(find_dotenv())
+
+'''
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 API_KEY = "cf39c9ce6431bd6c53143b1ed54dc663"
@@ -17,3 +24,42 @@ def find_weather(city: str) -> dict:
 
 output = find_weather('Cairo')
 print(output)
+
+'''
+
+# Define the output schema for the tool
+class WeatherForecast(BaseModel):
+    location: str
+    description: str
+    temperature_celsius: float
+
+weather_agent = Agent(
+    model='groq:llama-3.3-70b-versatile',
+    model_settings=ModelSettings(temperature=0.2),
+    output_type=str,
+    system_prompt=("You are a helpful weather assistant. Use 'get_weather_forecast' tool "
+    "to find current weather conditions for any city.Provide clean and friendly answers."),
+)
+
+# weather forecast tool
+@weather_agent.tool
+def get_weather_forecast(ctx: RunContext, city: str) -> WeatherForecast:
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    api_key = "cf39c9ce6431bd6c53143b1ed54dc663"
+    params = {
+        'q': city,
+        'appid': api_key,
+        'units': 'metric',
+    }
+    response = requests.get(url, params)
+    result = response.json()
+
+    return WeatherForecast(
+        location=result["name"],
+        description=result["weather"][0]["description"].capitalize(),
+        temperature_celsius=result["main"]["temp"]
+    )
+
+question = input("Ask about the weather:  ")
+response = weather_agent.run_sync(question)
+print("Forecast: ", response.output)
